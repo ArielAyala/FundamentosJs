3d3fa1e2b3fdb534862439b803fff92747e9e863
Ariel <arielayalamail@gmail.com>
2018-09-28 16:46:31 -0400

    Iniciando ciblos For

--- clase12.js
+++ clase12.js
@@ -48,9 +48,8 @@ function imprimirProfesiones (persona) {
 
 const MAYORIA_DE_EDAD = 18
 
-function esMayorDeEdad(persona) {
-  return persona.edad >= MAYORIA_DE_EDAD
-}
+const esMayorDeEdad = ({edad}) => edad >= MAYORIA_DE_EDAD
+
 
 
 function imprimirSiEsMayorDeEdad (persona) {
@@ -58,5 +57,11 @@ function imprimirSiEsMayorDeEdad (persona) {
     console.log(`${persona.nombre} es mayor de edad`)
   }else {
     console.log(`${persona.nombre} es menor de edad`);
+  }
+}
+
+function permitirAcceso(persona){
+  if (!esMayorDeEdad(persona)){
+    console.log('Acceso denegado');
   }
 }
---
+++ clase13.js
@@ -0,0 +1,8 @@
+var ariel = {
+  nombre: 'Ariel',
+  apellido: 'Ayala',
+  edad: 28,
+  peso: 75
+}
+
+console.log(`Al inicio del año ${ariel.nombre} pesa ${ariel.peso} kg`)
