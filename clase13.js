c38223fc8f7c30ed6f88bf7065e9259f8c4f6459
Ariel <arielayalamail@gmail.com>
2018-09-29 07:52:35 -0400

    TErminando ciclo for

--- clase13.js
+++ clase13.js
@@ -6,3 +6,19 @@ var ariel = {
 }
 
 console.log(`Al inicio del año ${ariel.nombre} pesa ${ariel.peso} kg`)
+
+const INCREMENTO_PESO = 200
+
+const aumentarDePeso = (persona, incremento) =>  persona.peso += INCREMENTO_PESO
+const adelgazar = persona => persona.peso -= INCREMENTO_PESO
+
+for (var i = 1; i <= 365; i++) {
+  var random = Math.random()
+
+  if (random < 0.25) {
+    aumentarDePeso(ariel)
+  } else if (random < 0.5){
+    adelgazar(ariel)
+  }
+}
+console.log(`Al final del año ${ariel.nombre} pesa ${ariel.peso.toFixed(1)} kg`)
